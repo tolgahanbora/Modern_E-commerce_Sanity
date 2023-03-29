25f625c43b23f96151dcd789d71365bb3b9c296f
tolgahanbora <tolgahanbora@outlook.com>
2023-03-29 22:30:41 +0300

    added netlify

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,15 @@ import React from 'react'
 import { Layout } from '../components'
 import { StateContext } from '@/context/StateContext'
 import {Toaster} from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react';
+
 export default function App({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
         <Toaster />
   <Component {...pageProps} />
+  <Analytics />
   </Layout>
   </StateContext>
   )
